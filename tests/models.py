2936af10500ef371d257a536ada65e3849acfe1b
sage <laymonage@gmail.com>
2020-07-02 16:10:07 +0700

    Copy model field tests from Django 3.1
    With necessary adjustments applied.

--- tests/models.py
+++ tests/models.py
@@ -1,7 +1,30 @@
+import json
+import uuid
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 
 from django_jsonfield_backport.models import JSONField
 
 
+class CustomJSONDecoder(json.JSONDecoder):
+    def __init__(self, object_hook=None, *args, **kwargs):
+        return super().__init__(object_hook=self.as_uuid, *args, **kwargs)
+
+    def as_uuid(self, dct):
+        if 'uuid' in dct:
+            dct['uuid'] = uuid.UUID(dct['uuid'])
+        return dct
+
+
 class JSONModel(models.Model):
-    data = JSONField()
+    value = JSONField()
+
+
+class NullableJSONModel(models.Model):
+    value = JSONField(blank=True, null=True)
+    value_custom = JSONField(
+        encoder=DjangoJSONEncoder,
+        decoder=CustomJSONDecoder,
+        null=True,
+    )
