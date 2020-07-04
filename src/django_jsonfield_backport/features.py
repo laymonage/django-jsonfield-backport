ce1693a88eaeca52256007927893d6bf7504a188
sage <laymonage@gmail.com>
2020-07-05 05:56:07 +0700

    Remove database features related with introspections
    Third party packages can't implement introspections yet.

--- src/django_jsonfield_backport/features.py
+++ src/django_jsonfield_backport/features.py
@@ -35,28 +35,11 @@ class MySQLFeatures(BaseMySQLFeatures):
             return self.connection.mysql_version >= (10, 2, 1)
         return self.connection.mysql_version >= (8, 0, 16)
 
-    supports_table_check_constraints = property(
-        operator.attrgetter("supports_column_check_constraints")
-    )
-
-    @cached_property
-    def can_introspect_check_constraints(self):
-        if self.connection.mysql_is_mariadb:
-            version = self.connection.mysql_version
-            return (version >= (10, 2, 22) and version < (10, 3)) or version >= (10, 3, 10,)
-        return self.connection.mysql_version >= (8, 0, 16)
-
     @cached_property
     def supports_json_field(self):
         if self.connection.mysql_is_mariadb:
             return self.connection.mysql_version >= (10, 2, 7)
         return self.connection.mysql_version >= (5, 7, 8)
-
-    @cached_property
-    def can_introspect_json_field(self):
-        if self.connection.mysql_is_mariadb:
-            return self.supports_json_field and self.can_introspect_check_constraints
-        return self.supports_json_field
 
     supports_primitives_in_json_field = True
     has_native_json_field = False
@@ -66,7 +49,6 @@ class MySQLFeatures(BaseMySQLFeatures):
 class OracleFeatures(BaseOracleFeatures):
     supports_json_field = True
     supports_primitives_in_json_field = False
-    can_introspect_json_field = True
     has_native_json_field = False
     has_json_operators = False
 
@@ -74,7 +56,6 @@ class OracleFeatures(BaseOracleFeatures):
 class PostgresFeatures(BasePostgresFeatures):
     supports_json_field = True
     supports_primitives_in_json_field = True
-    can_introspect_json_field = True
     has_native_json_field = True
     has_json_operators = True
 
@@ -90,7 +71,6 @@ class SQLiteFeatures(BaseSQLiteFeatures):
         return True
 
     supports_primitives_in_json_field = True
-    can_introspect_json_field = property(operator.attrgetter("supports_json_field"))
     has_native_json_field = False
     has_json_operators = False
 
